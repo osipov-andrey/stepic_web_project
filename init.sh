477263f06723479772d20e982fb81e7254e556c6
osipov-andrey <osyapov@bk.ru>
2019-11-20 09:16:14 +0000

    edit init.sh for launch gunicorn

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo pkill -f gunicorn
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf -b 0.0.0.0:8080 hello:app &
+
