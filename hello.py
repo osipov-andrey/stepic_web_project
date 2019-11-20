6e33b0c378d60cab16f660cb40b89df916824bd9
osipov-andrey <osyapov@bk.ru>
2019-11-20 09:23:46 +0000

    final(?) hello.py

--- hello.py
+++ hello.py
@@ -3,16 +3,9 @@
 
 def app(environ, start_response):
 	query = environ['QUERY_STRING']
-	#query_parse = urllib.parse.parse_qs(query)
-	#body = ''
-	#for i in query_parse:
-	#	for j in query_parse[i]:
-	#		body += '{}={}\n'.format(i, j)
-	#status = '200 OK'
+	#query = urllib.parse.parse_qs(environ['QUERY_STRING'])
+	status = '200 OK'
 	headers = [('Content-Type', 'text/plain')]
-	#body = '\n'.join([f'{i}={j}' for i in query_parse for j in query_parse[i]])
-	start_response('200 OK', headers)
-	#body = [bytes(i + '\n', 'ascii') for i in query.split('&')]
-	#body = bytes('\n'.join(environ.get('QUERY_STRING').split('&')), 'ascii')
+	start_response(status, headers)
 	body = [bytes('\r\n'.join(query.split('&')), encoding ='utf8')] 
 	return body
