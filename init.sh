b64a33036ac3104941c2fe8cbe982ff539de8ef7
Osipov Andrey Alekseevich <57837642+osipov-andrey@users.noreply.github.com>
2019-11-24 19:19:11 +0300

    Add files via upload
    Django

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
-sudo ln -sf /home/andrey/stepik/web-test/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/andrey/stepik/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo pkill -f gunicorn
-gunicorn -c ./gunicorn_conf.py hello:app &
+#gunicorn -c ./gunicorn_jango.py ask.wsgi:qa &
+gunicorn -c /home/andrey/stepik/web/etc/gunicorn_conf.py hello:app &
+cd ./ask
+gunicorn -c /home/andrey/stepik/web/etc/gunicorn_jango.py ask.wsgi:application &
+
