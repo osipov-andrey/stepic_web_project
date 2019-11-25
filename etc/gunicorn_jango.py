5224f06bfeb7e3863d4d1df32963683512e5ea9b
osipov-andrey <osyapov@bk.ru>
2019-11-25 13:02:08 +0000

    step 2.1 Done

--- etc/gunicorn_jango.py
+++ etc/gunicorn_jango.py
@@ -1,18 +1,15 @@
-#python = '/usr/bin/python3.7'
-#pythonpath = '/home/andrey/stepik/web/ask' 
-#working_dir = '/home/andrey/stepik/web/ask' 
-#bind = '0.0.0.0:8000'
-#workers = 5
+bind = '0.0.0.0:8000'
+workers = 5
 
 
-CONFIG = {
-        'mode': 'wsgi',
-        'working_dir': 'home/andrey/stepik/web/',
-        'pythonpath': '/usr/bin/python',
-        'args': (
-                '--bind=0.0.0.0:80',
-                '--workers=5',
-                '--timeout=60',
-                'hello:app'
-        )
-}
+#CONFIG = {
+#        'mode': 'wsgi',
+        #'working_dir': 'home/andrey/stepik/web/',
+#        'pythonpath': '/usr/bin/python3',
+#        'args': (
+#                '--bind=0.0.0.0:8000',
+#                '--workers=5',
+#                '--timeout=60',
+                #'hello:app'
+#        )
+#}
--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo /etc/init.d/nginx restart
 sudo pkill -f gunicorn
 gunicorn -c /home/box/web/etc/gunicorn_conf.py hello:app &
 cd ./ask
-#gunicorn -c /home/box/web/etc/gunicorn_jango.py ask.wsgi:application &
-gunicorn -b 0.0.0.0:8000 ask.wsgi &
+gunicorn -c /home/box/web/etc/gunicorn_jango.py ask.wsgi &
+#gunicorn -b 0.0.0.0:8000 ask.wsgi & # start from shell
