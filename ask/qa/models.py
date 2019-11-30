07e1c1d12fa24c556edb2ca2e5c77593df8c87f3
osipov-andrey <osyapov@bk.ru>
2019-11-30 13:55:42 +0300

    Create Database

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(default="")
+    added_at = models.DateField(default=timezone.now())
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author", on_delete=models.SET_DEFAULT, default="")
+    likes = models.ManyToManyField(User, related_name="question_like_user")
+    
+    def __str__(self):
+        return self.title
+    
+    def get_url(self):
+        return "/question/{}/".format(self.pk) 
+
+    #class Meta:
+    #    db_table = 'Questions'
+    #    ordering = ['-added_at']
+
+class Answer(models.Model):
+    text = models.TextField(default="")
+    addet_at = models.DateField(default=timezone.now())
+    question = models.ForeignKey(Question, default="", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default="")
+
+    def __str__(self):
+        return self.text
+
+    def get_url(self):
+        return "/question/{}/#answer_{}".format(self.question, self.id)
--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/andrey/stepik/web-mail/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo pkill -f gunicorn
-gunicorn -c /home/box/web/etc/gunicorn_conf.py hello:app &
+gunicorn -c /home/andrey/stepik/web-mail/etc/gunicorn_conf.py hello:app &
 cd ./ask
-gunicorn -c /home/box/web/etc/gunicorn_jango.py ask.wsgi &
-#gunicorn -b 0.0.0.0:8000 ask.wsgi & # start from shell
+gunicorn -c /home/andrey/stepik/web-mail/etc/gunicorn_jango.py ask.wsgi &
+sudo /etc/init.d/mysql restart
+mysql -u root -e "create database..."  # For Stepic
+#mysql -u root -p # For My PC
