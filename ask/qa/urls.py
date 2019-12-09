b15de6e5e0af42bdf0f1baa1272b41db7f08ffd8
osipov-andrey <osyapov@bk.ru>
2019-12-09 21:45:52 +0300

    Revert "for Step 2.3"
    This reverts commit 9c28254c18f4f17f7858c1b2c173cf6a5e77f0bc.
    
    Delete commit

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url
-from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='new'),
-    url(r'test/', views.test, name='test'),
-    path('popular/', views.popular, name='popular'),
-    path('question/<int:pk>/', views.question, name='question'),
+    url(r'^$', views.test, name='test'),
 ]
